d6d8db4334d22c73a304a95d719b11d5951681dd
Dfluan <dflaphp@gmail.com>
2017-12-21 23:58:14 -0200

    PDO * DAO - UPDATE/DELETE

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -103,6 +103,30 @@ Class Usuario
     }
   }
 
+  public function update($login, $password)
+  {
+    $this->setDeslogin($login);
+    $this->setDessenha($password);
+
+    $sql = new Sql();
+    $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+      ":LOGIN"=>$this->getDeslogin(),
+      ":PASSWORD"=>$this->getDessenha(),
+      ":ID"=>$this->getIdUsuario()
+    ));
+  }
+
+  public function delete($id)
+  {
+    $sql = new Sql;
+    $result = $sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+      ":ID"=>$id
+    ));
+    if ($result) {
+      echo "Delete com sucesso";
+    }
+  }
+
   public function __construct($login = "", $password = "")
   {
     $this->setDeslogin($login);
--- index.php
+++ index.php
@@ -18,11 +18,17 @@ require_once("config.php");
 // $user->login("Gabriela","142536");
 // echo $user;
 
-$user = new Usuario('Dflugann2018', '123456');
-$user->insert();
-echo $user;
+//INSERIR NOVO USUARIO
+// $user = new Usuario('Dflugann2018', '123456');
+// $user->insert();
+// echo $user;
 
+// ATUALIZAÇÃO DO REGISTRO DO USUARIO
+// $user = new Usuario();
+// $user->loadById(12);
+// $user->update("Gog", "123");
+// echo $user;
 
-
-
+$user = new Usuario();
+$user->delete(12);
  ?>
